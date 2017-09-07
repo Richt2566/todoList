0e3ef0997d47a85de3cae1e61e8d7a5a99b72b43
Rich T <rtranter25@gmail.com>
2017-09-07 10:21:52 -0500

    added intial files

---
+++ main.js
@@ -0,0 +1,54 @@
+document.getElementById("add").addEventListener("click", function() {
+	var value = document.getElementById("item").value;
+	if (value) 
+	addItemTodo(value);
+	document.getElementById("item").value = '';	
+
+});
+
+function removeItem() {
+	var item = this.parentNode.parentNode;
+	var parent = item.parentNode;
+
+	parent.removeChild(item);
+
+}
+
+
+function completeItem() {
+	var item = this.parentNode.parentNode;
+	var parent = item.parentNode;
+	var id = parent.id;
+
+	var target = (id === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
+	parent.removeChild(item);
+	target.insertBefore(item, target.childNodes[0]);
+	
+}
+
+
+function addItemTodo(text) {
+	var list = document.getElementById("todo");
+
+	var item = document.createElement("li");
+	item.innerText = text;
+
+	var buttons = document.createElement("div");
+	buttons.classList.add("buttons");
+
+	var remove = document.createElement("button");
+	remove.classList.add("remove");
+
+	remove.addEventListener("click", removeItem);
+
+	var complete = document.createElement("button");
+	complete.classList.add("complete");
+
+	complete.addEventListener("click", completeItem);
+
+	buttons.appendChild(remove);
+	buttons.appendChild(complete);
+	item.appendChild(buttons);
+	list.insertBefore(item, list.childNodes[0]);
+
+}
